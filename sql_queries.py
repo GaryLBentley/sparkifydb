bdf9a99816a67786aa5f2d216b93794a25b493a2
Bentley, Gary L <Gary.Bentley@usfoods.com>
2023-06-29 23:12:18 -0700

    Testing DROP and CREATE TABLE queries

--- sql_queries.py
+++ sql_queries.py
@@ -8,32 +8,46 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays \
-                         songplay_id serial PRIMARY KEY, \ ")
+songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays (\
+                         songplay_id int PRIMARY KEY, \
+                         start_time timestamp NOT NULL, \
+                         user_id int NOT NULL, \
+                         level varchar NOT NULL, \
+                         song_id varchar NOT NULL, \
+                         artist_id varchar NOT NULL, \
+                         session_id int NOT NULL, \
+                         location varchar NOT NULL, \
+                         user_agent varchar NOT NULL) ;")
 
-user_table_create = ("CREATE TABLE IF NOT EXISTS users \
+user_table_create = ("CREATE TABLE IF NOT EXISTS users (\
                      user_id int PRIMARY KEY, \
                      first_name varchar NOT NULL, \
                      last_name varchar NOT NULL, \
                      gender char(1) NOT NULL, \
-                     level varchar NOT NULL ;")
+                     level varchar NOT NULL);")
 
-song_table_create = ("CREATE TABLE IF NOT EXISTS songs \
+song_table_create = ("CREATE TABLE IF NOT EXISTS songs (\
                      song_id varchar PRIMARY KEY, \
                      title varchar NOT NULL, \
                      artist_id varchar NOT NULL, \
                      year int NOT NULL, \
-                     duration numeric NOT NULL;")
+                     duration numeric NOT NULL) ;")
 
-artist_table_create = ("CREATE TABLE IF NOT EXISTS artists \
+artist_table_create = ("CREATE TABLE IF NOT EXISTS artists (\
                      artist_id varchar PRIMARY KEY, \
                      name varchar NOT NULL, \
                      location varchar NOT NULL, \
                      latitude numeric, \
-                     longitude numeric ;")
+                     longitude numeric) ;")
 
-time_table_create = ("""
-""")
+time_table_create = ("CREATE TABLE IF NOT EXISTS time (\
+                     start_time timestamp PRIMARY KEY, \
+                     hour int NOT NULL, \
+                     day int NOT NULL \
+                     week int NOT NULL \
+                     month int NOT NULL \
+                     year int NOT NULL \
+                     weekday int NOT NULL) ;")
 
 # INSERT RECORDS
 
