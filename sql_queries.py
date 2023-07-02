c2115699031c72fe67d1a303d156c903f0d804af
Bentley, Gary L <Gary.Bentley@usfoods.com>
2023-07-02 12:11:42 -0700

    Created insert_table_queries list

--- sql_queries.py
+++ sql_queries.py
@@ -51,21 +51,57 @@ time_table_create = ("CREATE TABLE IF NOT EXISTS time (\
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = ("INSERT INTO songplays (\
+                         songplay_id, \
+                         start_time, \
+                         user_id, \
+                         level, \
+                         song_id, \
+                         artist_id, \
+                         session_id, \
+                         location, \
+                         user_agent) \
+                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) \
+                         ON CONFLICT DO NOTHING;")
 
-user_table_insert = ("""
-""")
+user_table_insert = ("INSERT INTO users (\
+                     user_id, \
+                     first_name, \
+                     last_name, \
+                     gender, \
+                     level) \
+                     VALUES (%s, %s, %s, %s, %s) \
+                     ON CONFLICT DO NOTHING;")
 
-song_table_insert = ("""
-""")
+song_table_insert = ("INSERT INTO songs (\
+                     song_id, \
+                     title, \
+                     artist_id, \
+                     year, \
+                     duration) \
+                     VALUES (%s, %s, %s, %s, %s) \
+                     ON CONFLICT DO NOTHING;")
 
-artist_table_insert = ("""
-""")
+artist_table_insert = ("INSERT INTO artists (\
+                       artist_id , \
+                       name, \
+                       location, \
+                       latitude, \
+                       longitude) \
+                       VALUES(%s, %s, %s, %s, %s) \
+                       ON CONFLICT DO NOTHING;")
 
 
-time_table_insert = ("""
-""")
+time_table_insert = ("INSERT INTO time (\
+                     start_time, \
+                     hour, \
+                     day, \
+                     week, \
+                     month, \
+                     year, \
+                     weekday) \
+                     VALUES(%s, %s, %s, %s, %s) \
+                     ON CONFLICT DO NOTHING;")
 
 # FIND SONGS
 
@@ -76,3 +112,4 @@ song_select = ("""
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
