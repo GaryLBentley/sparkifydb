3a52b16a5d18736853f2f0a0195757030e53def5
Bentley, Gary L <Gary.Bentley@usfoods.com>
2023-07-02 11:14:19 -0700

    Added error handling for create_database

--- create_tables.py
+++ create_tables.py
@@ -9,21 +9,63 @@ def create_database():
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
+    try:
+        conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    except psycopg2.Error as e:
+        print("Error: Unable to create connection to Postgres database - studentdb")
+        print(e)
     
+    # set the session autocommit = true
+    try:
+        conn.set_session(autocommit=True)
+    except psycopg2.Error as e:
+        print("Error: Unable to set autocommit to Postgres database - studentdb")
+        print(e)
+
+    # get the cursor
+    try:
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("Error: Unable to create cursor to Postgres database - studentdb")
+        print(e)
+    
+    # drop database sparkifydb if exists
+    try:
+        cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    except psycopg2.Error as e:
+        print("Error: Unable to drop database - sparkifydb")
+        print(e)
+
     # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    try:
+        cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    except psycopg2.Error as e:
+        print("Error: Unable to create database - sparkifydb")
+        print(e)
+
 
     # close connection to default database
-    conn.close()    
-    
+    try:
+        conn.close()    
+    except psycopg2.Error as e:
+        print("Error: Unable to close the connection to database - studentdb")
+        print(e)
+
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    cur = conn.cursor()
-    
+    try:
+        conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    except psycopg2.Error as e:
+        print("Error: Unable to connect to database - sparkifydb")
+        print(e)
+
+    # get cursor to sparkifydb
+    try:
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("Error: Unable to get cursor to database - sparkifydb")
+        print(e)
+
+    # return to caller with cur (cursor) and conn (connection)
     return cur, conn
 
 
